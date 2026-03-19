8cf91165e5f881a72e6b060dc7e348078d355e08
gjf2a <ferrer@hendrix.edu>
2026-03-19 16:57:00 -0500

    Changed API to enable creation either by invoking the command-line arguments from the environment or starting from nothing and building it up.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
 use std::{collections::HashMap, str::FromStr};
 
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone)]
 pub struct ArgVals {
     simple_vals: Vec<String>,
     mapped_vals: HashMap<String, String>,
 }
 
 impl ArgVals {
+    pub fn env() -> Self {
+        let mut result = Self::default();
+        for arg in std::env::args().skip(1) {
+            match arg.find('=') {
+                Some(eq) => result.add_mapped(&arg[..eq], &arg[eq+1..]),
+                None => result.add_simple(&arg),
+            }
+        }
+        result
+    }
+
+    pub fn add_simple(&mut self, simple: &str) {
+        self.simple_vals.push(simple.to_string());
+    }
+
+    pub fn add_mapped(&mut self, key: &str, value: &str) {
+        self.mapped_vals.insert(key.to_string(), value.to_string());
+    }
+
     pub fn len(&self) -> usize {
         self.simple_vals.len() + self.mapped_vals.len()
     }
@@ -51,28 +70,5 @@ impl ArgVals {
                 None
             }
         })
-    }
-}
-
-impl Default for ArgVals {
-    fn default() -> Self {
-        let mut simple_vals = vec![];
-        let mut mapped_vals = HashMap::default();
-        for arg in std::env::args().skip(1) {
-            if arg.contains("=") {
-                let parts = arg.split("=").collect::<Vec<_>>();
-                if parts.len() == 2 {
-                    mapped_vals.insert(parts[0].to_string(), parts[1].to_string());
-                } else {
-                    simple_vals.push(arg);
-                }
-            } else {
-                simple_vals.push(arg);
-            }
-        }
-        Self {
-            simple_vals,
-            mapped_vals,
-        }
     }
 }
