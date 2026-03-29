b8429e8274978bc9cc1756ce0f31e201db37ec67
gjf2a <ferrer@hendrix.edu>
2026-03-29 09:09:24 -0500

    Added ArgDocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, fmt::Display, str::FromStr};
+
+pub fn arg_param(arg: &str) -> Option<(&str, &str)> {
+    arg.find('=').map(|eq| (&arg[..eq], &arg[eq+1..]))
+}
 
 #[derive(Default, Debug, Clone)]
 pub struct ArgVals {
@@ -10,8 +14,8 @@ impl ArgVals {
     pub fn env() -> Self {
         let mut result = Self::default();
         for arg in std::env::args().skip(1) {
-            match arg.find('=') {
-                Some(eq) => result.add_mapped(&arg[..eq], &arg[eq+1..]),
+            match arg_param(&arg) {
+                Some((arg, value)) => result.add_mapped(arg, value),
                 None => result.add_simple(&arg),
             }
         }
@@ -74,5 +78,41 @@ impl ArgVals {
                 None
             }
         })
+    }
+}
+
+pub struct ArgDocs {
+    executable_name: String,
+    arg_defs: Vec<(String,String,Option<String>)>,
+}
+
+impl ArgDocs {
+    pub fn new(executable_name: &str, defs: &Vec<(&str,&str,&str)>) -> Self {
+        Self {
+            executable_name: executable_name.to_string(),
+            arg_defs: defs.iter().map(|(arg, arg_type, def)| (arg.to_string(), arg_type.to_string(), if def.len() == 0 {None} else {Some(def.to_string())})).collect(),
+        }
+    }
+}
+
+macro_rules! write_opt {
+    ($f:tt, $s:expr, $sp:expr) => {
+        $s.as_ref().map_or(Ok(()), |s| write!($f, "{}{s}", $sp))
+    };
+}
+
+impl Display for ArgDocs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Usage: {}", self.executable_name)?;
+        for (sym, def, sym_type) in self.arg_defs.iter().filter(|(arg,_,_)| arg_param(arg).is_none()) {
+            write!(f, " {sym} ({def})")?;
+            write_opt!(f, sym_type, ' ')?;
+            write!(f, " ")?;
+        }
+        for (assign, def, assign_type) in self.arg_defs.iter().filter(|(arg,_,_)| arg_param(arg).is_some()) {
+            write!(f, "\n\t{assign} ({def})")?;
+            write_opt!(f, assign_type, "\n\t\t")?;
+        }
+        Ok(())
     }
 }
