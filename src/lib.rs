af98721f48214bf9d5e4853a44495e20c5f0073a
gjf2a <ferrer@hendrix.edu>
2026-03-29 16:36:06 -0500

    Completely refactored to simplify - all command-line arguments are now assignments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 use std::{collections::HashMap, fmt::Display, str::FromStr};
 
-pub fn arg_param(arg: &str) -> Option<(&str, &str)> {
+pub fn assignment_param(arg: &str) -> Option<(&str, &str)> {
     arg.find('=').map(|eq| (&arg[..eq], &arg[eq+1..]))
 }
 
 #[derive(Default, Debug, Clone)]
 pub struct ArgVals {
-    simple_vals: Vec<String>,
     mapped_vals: HashMap<String, String>,
 }
 
@@ -14,36 +13,19 @@ impl ArgVals {
     pub fn env() -> Self {
         let mut result = Self::default();
         for arg in std::env::args().skip(1) {
-            match arg_param(&arg) {
-                Some((arg, value)) => result.add_mapped(arg, value),
-                None => result.add_simple(&arg),
+            if let Some((arg, value)) = assignment_param(&arg) {
+                result.add_mapping(arg, value);
             }
         }
         result
     }
 
-    pub fn add_simple(&mut self, simple: &str) {
-        self.simple_vals.push(simple.to_string());
-    }
-
-    pub fn add_mapped(&mut self, key: &str, value: &str) {
+    pub fn add_mapping(&mut self, key: &str, value: &str) {
         self.mapped_vals.insert(key.to_string(), value.to_string());
     }
 
     pub fn len(&self) -> usize {
-        self.simple_vals.len() + self.mapped_vals.len()
-    }
-
-    pub fn num_symbols(&self) -> usize {
-        self.simple_vals.len()
-    }
-
-    pub fn get_symbol(&self, i: usize) -> &str {
-        self.simple_vals[i].as_str()
-    }
-
-    pub fn has_symbol(&self, symbol: &str) -> bool {
-        self.simple_vals.iter().any(|s| s == symbol)
+        self.mapped_vals.len()
     }
 
     pub fn key_value_pairs<N: Copy + FromStr>(&self) -> impl Iterator<Item = (&str, N)> {
@@ -83,35 +65,58 @@ impl ArgVals {
 
 pub struct ArgDocs {
     executable_name: String,
-    arg_defs: Vec<(String,String,Option<String>)>,
+    arg2type_default: HashMap<String, (String,Option<String>)>,
+}
+
+pub fn merged_arg_docs<'a, A: Iterator<Item=&'a ArgDocs>>(executable_name: &str, arg_docs: A) -> ArgDocs {
+    let mut result = ArgDocs::new(executable_name, &vec![]);
+    for arg_doc in arg_docs {
+        for (arg, (arg_type, arg_default)) in arg_doc.arg2type_default.iter() {
+            match result.arg2type_default.get_mut(arg) {
+                None => {
+                    result.arg2type_default.insert(arg.to_string(), (arg_type.to_string(), arg_default.as_ref().map(|s| s.to_string())));
+                }
+                Some((_, prev_default)) => {
+                    if prev_default.is_none() {
+                        *prev_default = arg_default.as_ref().map(|s| s.clone());
+                    }
+                }
+            }
+        }
+    }
+    result
 }
 
 impl ArgDocs {
     pub fn new(executable_name: &str, defs: &Vec<(&str,&str,&str)>) -> Self {
         Self {
             executable_name: executable_name.to_string(),
-            arg_defs: defs.iter().map(|(arg, arg_type, def)| (arg.to_string(), arg_type.to_string(), if def.len() == 0 {None} else {Some(def.to_string())})).collect(),
+            arg2type_default: defs.iter().map(|(arg, arg_type, def)| (arg.to_string(), (arg_type.to_string(), if def.len() == 0 {None} else {Some(def.to_string())}))).collect(),
         }
     }
-}
 
-macro_rules! write_opt {
-    ($f:tt, $s:expr, $sp:expr) => {
-        $s.as_ref().map_or(Ok(()), |s| write!($f, "{}{s}", $sp))
-    };
+    pub fn get_args_with_defaults(&self) -> ArgVals {
+        let mut result = ArgVals::env();
+        for (arg, (_, def)) in self.arg2type_default.iter() {
+            if assignment_param(arg).is_some() && result.get_str_value(arg).is_none() {
+                if let Some(default_def) = def {
+                    result.add_mapping(arg, default_def);
+                }
+            } 
+        }
+        result
+    }
 }
 
 impl Display for ArgDocs {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Usage: {}", self.executable_name)?;
-        for (sym, def, sym_type) in self.arg_defs.iter().filter(|(arg,_,_)| arg_param(arg).is_none()) {
-            write!(f, " {sym} ({def})")?;
-            write_opt!(f, sym_type, ' ')?;
-            write!(f, " ")?;
-        }
-        for (assign, def, assign_type) in self.arg_defs.iter().filter(|(arg,_,_)| arg_param(arg).is_some()) {
-            write!(f, "\n\t{assign} ({def})")?;
-            write_opt!(f, assign_type, "\n\t\t")?;
+        for (assign, (assign_type, def)) in self.arg2type_default.iter() {
+            write!(f, "\t\n{assign}={assign_type}")?;
+            if let Some(d) = def {
+                write!(f, " [{d}]")?;
+            }
+            writeln!(f)?;
         }
         Ok(())
     }
